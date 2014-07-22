b914a3387e02f394b0dd651cf2e2df1b33a60eec
purplefox <timvolpe@gmail.com>
2014-07-22 14:09:55 +0100

    Use new Vertx API

--- src/test/java/io/vertx/test/ExampleRunner.java
+++ src/test/java/io/vertx/test/ExampleRunner.java
@@ -9,7 +9,7 @@ import io.vertx.core.Vertx;
 public class ExampleRunner {
 
   public static void main(String[] args) {
-    Vertx vertx = Vertx.newVertx();
+    Vertx vertx = Vertx.vertx();
     vertx.deployVerticle("js:examples/http_server", new DeploymentOptions(), ar -> {
       if (ar.succeeded()) {
         System.out.println("Succeeded in deploying");
