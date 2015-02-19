98c13690d5ef2474dbf18774d57a786e6fca2e0f
Julien Viet <julien@julienviet.com>
2015-02-19 20:26:06 +0100

    FOO

--- src/test/resources/async_test_verticle.js
+++ src/test/resources/async_test_verticle.js
@@ -1,16 +1,22 @@
 
 exports.vertxStartAsync = function(startFuture) {
-  vertx.setTimer(1100, function() {
-    startFuture.complete();
-  });
+  if (startFuture._jdel === undefined) {
+    startFuture.fail("Was expecting a proxy and not a native object");
+  } else {
+    vertx.setTimer(1100, function() {
+      startFuture.complete();
+    });
+  }
 };
 
 exports.vertxStopAsync = function(stopFuture) {
-
-  vertx.setTimer(1100, function() {
-    stopFuture.complete();
-  });
-
-}
+  if (stopFuture._jdel === undefined) {
+    stopFuture.fail("Was expecting a proxy and not a native object");
+  } else {
+    vertx.setTimer(1100, function() {
+      stopFuture.complete();
+    });
+  }
+};
 
 
