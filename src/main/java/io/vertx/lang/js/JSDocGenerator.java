99baba6a6bdc304ddb70bd9ed6a559c51ae0e778
Julien Viet <julien@julienviet.com>
2015-04-14 09:58:21 +0200

    Remove the js docgen callbacks and instead provide a DocGenerator implementation

---
+++ src/main/java/io/vertx/lang/js/JSDocGenerator.java
@@ -0,0 +1,110 @@
+package io.vertx.lang.js;
+
+import io.vertx.codegen.Case;
+import io.vertx.codegen.ClassKind;
+import io.vertx.codegen.TypeInfo;
+import io.vertx.codetrans.CodeTranslator;
+import io.vertx.codetrans.JavaScriptLang;
+import io.vertx.docgen.Coordinate;
+import io.vertx.docgen.DocGenerator;
+
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+
+/**
+ * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
+ */
+public class JSDocGenerator implements DocGenerator {
+
+  private TypeInfo.Factory factory;
+  private CodeTranslator translator;
+  private ProcessingEnvironment env;
+
+  @Override
+  public void init(ProcessingEnvironment processingEnv) {
+    factory = new TypeInfo.Factory(processingEnv.getElementUtils(), processingEnv.getTypeUtils());
+    translator = new CodeTranslator(processingEnv);
+    env = processingEnv;
+  }
+
+  @Override
+  public String getName() {
+    return "js";
+  }
+
+  @Override
+  public String renderSource(ExecutableElement elt, String source) {
+    JavaScriptLang lang = new JavaScriptLang();
+    try {
+      return translator.translate(elt, lang);
+    } catch (Exception e) {
+      System.out.println("Cannot generate " + elt.getEnclosingElement().getSimpleName() + "#" + elt.getSimpleName() + " : " + e.getMessage());
+      return "Code not translatable";
+    }
+  }
+
+  @Override
+  public String resolveTypeLink(TypeElement elt, Coordinate coordinate) {
+    TypeInfo type = factory.create(elt.asType());
+    if (type.getKind() == ClassKind.DATA_OBJECT) {
+      String baselink;
+      if (coordinate == null) {
+        baselink = "../";
+      } else {
+        baselink = "../../" + coordinate.getArtifactId() + "/";
+      }
+      return baselink + "cheatsheet/" + elt.getSimpleName().toString() + ".html";
+    }
+    if (type.getKind() == ClassKind.API) {
+      String baselink = "";
+      if (coordinate != null) {
+        baselink = "../../" + coordinate.getArtifactId() + "/js/";
+      }
+      return baselink + "jsdoc/" + Case.SNAKE.format(Case.CAMEL.parse(elt.getSimpleName().toString())) + "-" + elt.getSimpleName() + ".html";
+    }
+    return null;
+  }
+
+  @Override
+  public String resolveMethodLink(ExecutableElement elt, Coordinate coordinate) {
+    TypeElement typeElt = (TypeElement) elt.getEnclosingElement();
+    String link =  resolveTypeLink(typeElt, coordinate);
+    if (link != null) {
+      if (link.contains("cheatsheet")) {
+        link = link + '#' + java.beans.Introspector.decapitalize(elt.getSimpleName().toString().substring(3));
+      } else {
+        link = link + '#' + elt.getSimpleName();
+      }
+    }
+    return link;
+  }
+
+  @Override
+  public String resolveLabel(Element elt, String defaultLabel) {
+    if (elt.getKind() == ElementKind.METHOD) {
+      TypeInfo type = factory.create(elt.getEnclosingElement().asType());
+      if (type.getKind() == ClassKind.DATA_OBJECT) {
+        String name = elt.getSimpleName().toString();
+        if (name.startsWith("set") && name.length() > 3 && Character.isUpperCase(name.charAt(3))) {
+          name = java.beans.Introspector.decapitalize(name.substring(3));
+        }
+        return name;
+      }
+    }
+    return defaultLabel;
+  }
+
+  @Override
+  public String resolveConstructorLink(ExecutableElement elt, Coordinate coordinate) {
+    return "todo";
+  }
+
+  @Override
+  public String resolveFieldLink(VariableElement elt, Coordinate coordinate) {
+    return "todo";
+  }
+}
