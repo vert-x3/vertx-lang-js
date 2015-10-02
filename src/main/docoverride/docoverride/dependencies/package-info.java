a2d028313bd03d80a3db8dc5b331922a085e7e7e
Clement Escoffier <clement.escoffier@gmail.com>
2015-10-02 17:25:52 +0200

    Replace asciidoc attributes by docgen variables
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

--- src/main/docoverride/docoverride/dependencies/package-info.java
+++ src/main/docoverride/docoverride/dependencies/package-info.java
@@ -25,12 +25,12 @@
  * <dependency>
  *  <groupId>io.vertx</groupId>
  *  <artifactId>vertx-core</artifactId>
- *  <version>{maven-version}</version>
+ *  <version>${maven.version}</version>
  * </dependency>
  * <dependency>
- *   <groupId>{maven-groupId}</groupId>
- *   <artifactId>{maven-artifactId}</artifactId>
- *   <version>{maven-version}</version>
+ *   <groupId>${maven.groupId}</groupId>
+ *   <artifactId>${maven.artifactId}</artifactId>
+ *   <version>${maven.version}</version>
  * </dependency>
  * ----
  *
@@ -38,8 +38,8 @@
  *
  * [source,groovy,subs="+attributes"]
  * ----
- * compile "io.vertx:vertx-core:{maven-version}"
- * compile "{maven-groupId}:{maven-artifactId}:{maven-version}"
+ * compile "io.vertx:vertx-core:${maven.version}"
+ * compile "${maven.groupId}:${maven.artifactId}:${maven.version}"
  * ----
  */
 @Document(fileName = "override/dependencies.adoc")
