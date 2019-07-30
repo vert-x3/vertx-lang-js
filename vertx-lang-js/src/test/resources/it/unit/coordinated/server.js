ec742723010b4778c4da8af930d2c1b8c7e7f94c
Julien Viet <julien@julienviet.com>
2019-07-30 09:04:07 +0200

    The test should use end(s) instead of write(s).end() since fluency breaks with write

--- vertx-lang-js/src/test/resources/it/unit/coordinated/server.js
+++ vertx-lang-js/src/test/resources/it/unit/coordinated/server.js
@@ -1,7 +1,7 @@
 exports.vertxStartAsync = function(startFuture) {
   var server = vertx.createHttpServer({"port":8080,"host":"localhost"});
   server.requestStream().handler(function(req) {
-    req.response().setChunked(true).write("Hello World").end();
+    req.response().setChunked(true).end("Hello World");
   });
   server.listen(function(ar) {
     startFuture.complete();
