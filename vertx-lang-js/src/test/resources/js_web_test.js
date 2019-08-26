c7efcdff78ba4491b937c62cb774b61b5461787a
Julien Viet <julien@julienviet.com>
2019-08-26 16:34:39 +0200

    Add vertx-web cookie IT test

---
+++ vertx-lang-js/src/test/resources/js_web_test.js
@@ -0,0 +1,42 @@
+var TimeUnit = java.util.concurrent.TimeUnit;
+var Assert = org.junit.Assert;
+var CountDownLatch = java.util.concurrent.CountDownLatch;
+var Vertx = require("vertx-js/vertx");
+var Router = require("vertx-web-js/router");
+var Cookie = require("vertx-js/cookie");
+
+function testCookies() {
+
+  var latch = new CountDownLatch(1);
+  var vertx = Vertx.vertx();
+
+  var cookie1 = null;
+  var router = Router.router(vertx);
+  router.route().handler(function (routingContext) {
+    var response = routingContext.response();
+    cookie1 = routingContext.getCookie("cookie1");
+    routingContext.addCookie(Cookie.cookie("cookie2", "apple"));
+    response.end("Hello World from Vert.x-Web!");
+  });
+  var server = vertx.createHttpServer();
+  server.requestHandler(router.handle).listen(8080);
+
+  var client = vertx.createHttpClient();
+  var cookie2 = null;
+  var req = client.get(8080, "localhost", "/some-uri", function (response) {
+    console.log("Received response with status code " + response.statusCode());
+    cookie2 = response.getHeader("set-cookie");
+    latch.countDown();
+  });
+  req.putHeader("cookie", "cookie1=banana");
+  req.end();
+
+  Assert.assertTrue(latch.await(2, TimeUnit.MINUTES));
+  vertx.close();
+  Assert.assertNotNull(cookie1);
+  Assert.assertEquals("cookie1", cookie1.getName());
+  Assert.assertEquals("banana", cookie1.getValue());
+  Assert.assertEquals("cookie2=apple", cookie2);
+}
+
+this[testName]();
